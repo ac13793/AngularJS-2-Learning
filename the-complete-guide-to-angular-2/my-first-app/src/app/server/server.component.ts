2995b09bd603d25ed03b0a4fb72207ba63b14052
Ankit Chaurasia <ac13793@gmail.com>
2017-12-15 10:30:02 +0530

    Creation and using of component

---
+++ the-complete-guide-to-angular-2/my-first-app/src/app/server/server.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+// Store metadata for the class in the background which will tell angular what to do with this class
+@Component({
+    selector: 'app-server',
+    templateUrl: './server.component.html'
+})
+
+export class ServerComponent {
+
+}
