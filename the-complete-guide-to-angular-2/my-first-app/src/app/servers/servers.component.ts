4593e2913bcd33aa9d51a23b2394f8a6b36c630b
Ankit Chaurasia <ac13793@gmail.com>
2017-12-16 16:02:16 +0530

    Component Selectors

--- the-complete-guide-to-angular-2/my-first-app/src/app/servers/servers.component.ts
+++ the-complete-guide-to-angular-2/my-first-app/src/app/servers/servers.component.ts
@@ -2,16 +2,10 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  // templateUrl: './servers.component.html',
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>`,
-  // styleUrls: ['./servers.component.css'],
-  styles: [`
-    h3 {
-      color: blue
-    }
-  `]
+  // selector: '[app-servers]',
+  // selector: '.app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
 
