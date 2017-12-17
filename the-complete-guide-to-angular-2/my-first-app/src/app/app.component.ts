04d1c1e88468c8bf74695700a75937b8bd153e28
Ankit Chaurasia <ac13793@gmail.com>
2017-12-17 17:35:12 +0530

    binding to custom event

--- the-complete-guide-to-angular-2/my-first-app/src/app/app.component.ts
+++ the-complete-guide-to-angular-2/my-first-app/src/app/app.component.ts
@@ -6,6 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
   serverElements = [{type: 'server', name: 'test server', content: 'test content'}];
+
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(bluePrintData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: bluePrintData.serverName,
+      content: bluePrintData.serverContent
+    });
+  }
 }
