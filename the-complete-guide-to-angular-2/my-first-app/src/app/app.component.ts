f8ea0d602860b98aa5afab847f8859773658c54a
Ankit Chaurasia <ac13793@gmail.com>
2017-12-19 22:13:32 +0530

    ngFor and ngIf Recap

--- the-complete-guide-to-angular-2/my-first-app/src/app/app.component.ts
+++ the-complete-guide-to-angular-2/my-first-app/src/app/app.component.ts
@@ -5,22 +5,29 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+// export class AppComponent {
+//   serverElements = [{type: 'server', name: 'test server', content: 'test content'}];
+
+//   onServerAdded(serverData: {serverName: string, serverContent: string}) {
+//     this.serverElements.push({
+//       type: 'server',
+//       name: serverData.serverName,
+//       content: serverData.serverContent
+//     });
+//   }
+
+//   onBlueprintAdded(bluePrintData: {serverName: string, serverContent: string}) {
+//     this.serverElements.push({
+//       type: 'blueprint',
+//       name: bluePrintData.serverName,
+//       content: bluePrintData.serverContent
+//     });
+//   }
+// }
+
 export class AppComponent {
-  serverElements = [{type: 'server', name: 'test server', content: 'test content'}];
-
-  onServerAdded(serverData: {serverName: string, serverContent: string}) {
-    this.serverElements.push({
-      type: 'server',
-      name: serverData.serverName,
-      content: serverData.serverContent
-    });
-  }
-
-  onBlueprintAdded(bluePrintData: {serverName: string, serverContent: string}) {
-    this.serverElements.push({
-      type: 'blueprint',
-      name: bluePrintData.serverName,
-      content: bluePrintData.serverContent
-    });
-  }
+  // numbers = [1, 2, 3, 4, 5];
+  oddNumbers = [1, 3, 5, 7];
+  evenNumbers = [2, 4, 6]
+  onlyOdd = false;
 }
