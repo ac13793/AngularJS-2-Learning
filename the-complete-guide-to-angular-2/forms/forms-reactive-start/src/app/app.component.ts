6bc2168aa00a64b66c8577c2cf412923cc7edd02
Ankit Chaurasia <ac13793@gmail.com>
2018-01-21 22:07:32 +0530

    Reactive Using Error Codes

--- the-complete-guide-to-angular-2/forms/forms-reactive-start/src/app/app.component.ts
+++ the-complete-guide-to-angular-2/forms/forms-reactive-start/src/app/app.component.ts
@@ -13,8 +13,8 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
-        'username': new FormControl(null, Validators.required),
-        'email': new FormControl(null, [Validators.required, Validators.email, this.forbiddenNames.bind(this)]),
+        'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
+        'email': new FormControl(null, [Validators.required, Validators.email]),
       }),
       'gender': new FormControl('male'),
       'hobbies': new FormArray([])
