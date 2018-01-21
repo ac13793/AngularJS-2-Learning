4e6169d57a45fb1bc08f7fba9bea2ee34ba91f80
Ankit Chaurasia <ac13793@gmail.com>
2018-01-21 17:05:34 +0530

    TD Set Default Values with ngModel Property Binding

--- the-complete-guide-to-angular-2/forms/forms-td-start/src/app/app.component.ts
+++ the-complete-guide-to-angular-2/forms/forms-td-start/src/app/app.component.ts
@@ -8,6 +8,8 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
   @ViewChild('f') signupForm: NgForm;  // @ViewChild allows us to access local reference
+  defaultQuestion = 'pet';
+
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
