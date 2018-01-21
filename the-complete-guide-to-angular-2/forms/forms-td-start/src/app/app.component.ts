acefaee17bd855a6a920181b1e3540935eb6a422
Ankit Chaurasia <ac13793@gmail.com>
2018-01-21 17:11:44 +0530

    TD Using ngModel with Two-Way-Binding

--- the-complete-guide-to-angular-2/forms/forms-td-start/src/app/app.component.ts
+++ the-complete-guide-to-angular-2/forms/forms-td-start/src/app/app.component.ts
@@ -9,6 +9,7 @@ import { NgForm } from '@angular/forms';
 export class AppComponent {
   @ViewChild('f') signupForm: NgForm;  // @ViewChild allows us to access local reference
   defaultQuestion = 'pet';
+  answer = '';
 
   suggestUserName() {
     const suggestedName = 'Superuser';
