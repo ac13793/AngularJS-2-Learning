daf567ab9f5806d7d200488b82c489f2e79b8a1d
Ankit Chaurasia <ac13793@gmail.com>
2018-01-25 23:19:36 +0530

    Transform Responses Easily with Observable Operators map

--- the-complete-guide-to-angular-2/http-start/src/app/server.service.ts
+++ the-complete-guide-to-angular-2/http-start/src/app/server.service.ts
@@ -1,21 +1,30 @@
 import { Injectable } from "@angular/core";
 import { Http, Headers } from "@angular/http";
+import 'rxjs/Rx';
+import { Response } from "@angular/http";
 
 // This decorator is required if you plan to inject a service into a service
 @Injectable()
 export class ServerService {
-    constructor(private http: Http){}
+    constructor(private http: Http) { }
 
-    storeServers(servers: any[]){
+    storeServers(servers: any[]) {
         const headers = new Headers({
             'Content-Type': 'application/json'
         });
         // return this.http.post('https://udemy-ng-http-f79ae.firebaseio.com/data.json', servers, {headers: headers});
         // Put request does not create new data instead it will override the existing data
-        return this.http.put('https://udemy-ng-http-f79ae.firebaseio.com/data.json', servers, {headers: headers});
+        return this.http.put('https://udemy-ng-http-f79ae.firebaseio.com/data.json', servers, { headers: headers });
     }
 
-    getServers(){
-        return this.http.get('https://udemy-ng-http-f79ae.firebaseio.com/data.json');
+    getServers() {
+        return this.http.get('https://udemy-ng-http-f79ae.firebaseio.com/data.json')
+            .map(
+            (response: Response) => {
+                const data = response.json();
+                // Here we are returning another Observables
+                return data;
+            }
+            );
     }
 }
