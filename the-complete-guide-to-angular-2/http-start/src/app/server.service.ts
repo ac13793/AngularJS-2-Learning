a7ba3df539336c5ca01e1a7c2518abce46493bca
Ankit Chaurasia <ac13793@gmail.com>
2018-01-25 20:40:22 +0530

    Adjusting Request Headers

--- the-complete-guide-to-angular-2/http-start/src/app/server.service.ts
+++ the-complete-guide-to-angular-2/http-start/src/app/server.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { Http } from "@angular/http";
+import { Http, Headers } from "@angular/http";
 
 // This decorator is required if you plan to inject a service into a service
 @Injectable()
@@ -7,6 +7,9 @@ export class ServerService {
     constructor(private http: Http){}
 
     storeServers(servers: any[]){
-        return this.http.post('https://udemy-ng-http-f79ae.firebaseio.com/data.json', servers);
+        const headers = new Headers({
+            'Content-Type': 'application/json'
+        });
+        return this.http.post('https://udemy-ng-http-f79ae.firebaseio.com/data.json', servers, {headers: headers});
     }
 }
