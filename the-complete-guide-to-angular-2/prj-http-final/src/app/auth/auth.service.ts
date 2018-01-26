055aed895baeed5baab95c252aa70003804f70e1
Ankit Chaurasia <ac13793@gmail.com>
2018-01-26 19:05:06 +0530

    Signing Users Up

--- the-complete-guide-to-angular-2/prj-http-final/src/app/auth/auth.service.ts
+++ the-complete-guide-to-angular-2/prj-http-final/src/app/auth/auth.service.ts
@@ -2,6 +2,9 @@ import * as firebase from 'firebase';
 
 export class AuthService{
     signupUser(email: string, password: string){
-        firebase.auth().createUserWithEmailAndPassword();
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+        .catch(
+            error => console.log(error)
+        );
     }
 }
