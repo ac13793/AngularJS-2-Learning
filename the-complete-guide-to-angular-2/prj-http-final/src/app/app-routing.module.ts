0a143d81879b1d796fa319718a5fcabe926dac4c
Ankit Chaurasia <ac13793@gmail.com>
2018-01-26 19:16:53 +0530

    Signin Users In

--- the-complete-guide-to-angular-2/prj-http-final/src/app/app-routing.module.ts
+++ the-complete-guide-to-angular-2/prj-http-final/src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.compon
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { SignupComponent } from 'app/auth/signup/signup.component';
+import { SigninComponent } from 'app/auth/signin/signin.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
@@ -19,7 +20,8 @@ const appRoutes: Routes = [
     ]
   },
   { path: 'shopping-list', component: ShoppingListComponent },
-  { path: 'signup', component: SignupComponent }
+  { path: 'signup', component: SignupComponent },
+  { path: 'signin', component: SigninComponent }
 ];
 
 @NgModule({
