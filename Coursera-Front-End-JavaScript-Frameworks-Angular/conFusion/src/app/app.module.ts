180154559e959482920b49ca3f249b57c3ded5cf
Ankit Chaurasia <ac13793@gmail.com>
2018-02-17 14:43:49 +0530

    Reactive Forms Part 1

--- Coursera-Front-End-JavaScript-Frameworks-Angular/conFusion/src/app/app.module.ts
+++ Coursera-Front-End-JavaScript-Frameworks-Angular/conFusion/src/app/app.module.ts
@@ -15,7 +15,7 @@ import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { PromotionService } from './services/promotion.service';
 import { LeaderService } from './services/leader.service';
@@ -41,7 +41,8 @@ import { LoginComponent } from './login/login.component';
     BrowserAnimationsModule,
     MaterialModule,
     FlexLayoutModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [DishService, PromotionService, LeaderService],
   entryComponents: [LoginComponent],
