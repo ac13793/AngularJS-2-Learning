baf4616d69217585e6384eb3fa5cc939d135f308
Ankit Chaurasia <ac13793@gmail.com>
2018-02-18 16:12:29 +0530

    REST

--- Coursera-Front-End-JavaScript-Frameworks-Angular/conFusion/src/app/app.module.ts
+++ Coursera-Front-End-JavaScript-Frameworks-Angular/conFusion/src/app/app.module.ts
@@ -22,7 +22,8 @@ import { LeaderService } from './services/leader.service';
 import { LoginComponent } from './login/login.component';
 import { baseURL } from './shared/baseurl';
 import { ProcessHTTPMsgService } from './services/process-httpmsg.service';
-
+import { RestangularModule, Restangular } from 'ngx-restangular';
+import { RestangularConfigFactory } from './shared/restConfig';
 
 @NgModule({
   declarations: [
@@ -44,7 +45,8 @@ import { ProcessHTTPMsgService } from './services/process-httpmsg.service';
     MaterialModule,
     FlexLayoutModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RestangularModule.forRoot(RestangularConfigFactory)
   ],
   providers: [DishService, PromotionService, LeaderService, { provide: 'BaseURL', useValue: baseURL }, ProcessHTTPMsgService],
   entryComponents: [LoginComponent],
--- Coursera-Front-End-JavaScript-Frameworks-Angular/conFusion/src/app/services/dish.service.ts
+++ Coursera-Front-End-JavaScript-Frameworks-Angular/conFusion/src/app/services/dish.service.ts
@@ -9,34 +9,30 @@ import { baseURL } from '../shared/baseurl';
 import { ProcessHTTPMsgService } from './process-httpmsg.service';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
+import { RestangularModule, Restangular } from 'ngx-restangular';
 
 @Injectable()
 export class DishService {
 
-  constructor(private http: Http,
-    private processHTTPMsgService: ProcessHTTPMsgService) { }
+  constructor(private restangular: Restangular,
+    private processHTTPMsg: ProcessHTTPMsgService) { }
 
   getDishes(): Observable<Dish[]> {
-    return this.http.get(baseURL + 'dishes')
-      .map(res => this.processHTTPMsgService.extractData(res))
-      .catch(error => this.processHTTPMsgService.handleError(error));
+    return this.restangular.all('dishes').getList();
   }
 
   getDish(id: number): Observable<Dish> {
-    return this.http.get(baseURL + 'dishes/' + id)
-      .map(res => this.processHTTPMsgService.extractData(res))
-      .catch(error => this.processHTTPMsgService.handleError(error));
+    return this.restangular.one('dishes', id).get();
   }
 
   getFeaturedDish(): Observable<Dish> {
-    return this.http.get(baseURL + 'dishes?featured=true')
-      .map(res => this.processHTTPMsgService.extractData(res)[0])
-      .catch(error => this.processHTTPMsgService.handleError(error));
+    return this.restangular.all('dishes').getList({ featured: true })
+      .map(dishes => dishes[0]);
   }
 
   getDishIds(): Observable<number[]> {
     return this.getDishes()
-      .map(dishes => { dishes.map(dish => dish.id); })
-      .catch(error => this.processHTTPMsgService.handleError(error));
+      .map(dishes => { return dishes.map(dish => dish.id) })
+      .catch(error => { return error; });
   }
 }
